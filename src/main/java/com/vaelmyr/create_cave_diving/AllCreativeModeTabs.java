60b3e8dbd1b3e0917f2c133dfc3bc256288a8aae
Vaelmyr <sdistefano.dev@gmail.com>
2026-08-21 14:19:36 +0200

    fix: fixed creative tab display

---
+++ src/main/java/com/vaelmyr/create_cave_diving/AllCreativeModeTabs.java
@@ -0,0 +1,31 @@
+package com.vaelmyr.create_cave_diving;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public final class AllCreativeModeTabs {
+    private static final DeferredRegister<CreativeModeTab> REGISTER =
+        DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CreateCaveDiving.MODID);
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> BASE = REGISTER.register("base",
+        () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.create_cave_diving.base"))
+            .withTabsBefore(com.simibubi.create.AllCreativeModeTabs.PALETTES_CREATIVE_TAB.getKey())
+            .icon(AllItems.COPPER_RESPIRATOR::asStack)
+            .displayItems((parameters, output) -> {
+                output.accept(AllItems.COPPER_RESPIRATOR.get());
+                output.accept(AllItems.NETHERITE_RESPIRATOR.get());
+            })
+            .build());
+
+    public static void register(IEventBus modEventBus) {
+        REGISTER.register(modEventBus);
+    }
+
+    private AllCreativeModeTabs() {
+    }
+}
--- src/main/java/com/vaelmyr/create_cave_diving/CreateCaveDiving.java
+++ src/main/java/com/vaelmyr/create_cave_diving/CreateCaveDiving.java
@@ -5,7 +5,9 @@ import com.mojang.logging.LogUtils;
 import com.tterrag.registrate.Registrate;
 import com.vaelmyr.create_cave_diving.config.ServerConfig;
 
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
@@ -20,7 +22,8 @@ public class CreateCaveDiving {
 
     private static final StackWalker STACK_WALKER = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
 
-    private static final Registrate REGISTRATE = Registrate.create(MODID);
+    private static final Registrate REGISTRATE = Registrate.create(MODID)
+        .defaultCreativeTab((ResourceKey<CreativeModeTab>) null);
 
     public CreateCaveDiving(IEventBus modEventBus, ModContainer modContainer) {
         LOGGER.info("{} initializing!", NAME);
@@ -28,6 +31,7 @@ public class CreateCaveDiving {
         modContainer.registerConfig(ModConfig.Type.SERVER, ServerConfig.SPEC);
 
         AllItems.register();
+        AllCreativeModeTabs.register(modEventBus);
     }
 
     public static ResourceLocation asResource(String path) {
